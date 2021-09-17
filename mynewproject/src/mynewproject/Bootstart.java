6ad5c089700e78bfef28903ce5b0964d01762544
Shubhendu1234 <skj2000786@gmail.com>
2021-09-17 16:07:21 +0530

    my first commit

---
+++ mynewproject/src/mynewproject/Bootstart.java
@@ -0,0 +1,10 @@
+package mynewproject;
+
+public class Bootstart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from STS");
+	}
+
+}
