62df527cc3ca312641b0d93c4bc5f7475282e5e3
Shubhendu1234 <skj2000786@gmail.com>
2021-09-20 11:48:47 +0530

    sum added in boot array

--- mynewproject/src/mynewproject/Bootstart.java
+++ mynewproject/src/mynewproject/Bootstart.java
@@ -5,6 +5,16 @@ public class Bootstart {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from STS");
+		show();
+	}
+	public static void show()
+	{
+		int[] a= {1,2,3,4,5};
+		int sum=0;
+		System.out.println(sum);
+		for(int n:a)
+			sum=sum+a[n]
+		System.out.println(sum);
 	}
 
 }
