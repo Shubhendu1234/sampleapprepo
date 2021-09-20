4ca1302d8b36126b702fad5730881dc093204b9e
Shubhendu1234 <skj2000786@gmail.com>
2021-09-20 11:44:12 +0530

    array is added in boot

--- mynewproject/src/mynewproject/Bootstart.java
+++ mynewproject/src/mynewproject/Bootstart.java
@@ -5,6 +5,11 @@ public class Bootstart {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from STS");
+		show();
+	}
+	public static void show() {
+		int[] a= {1,2,3,4,5,6};
+		System.out.println("hello from sts");
 	}
 
 }
